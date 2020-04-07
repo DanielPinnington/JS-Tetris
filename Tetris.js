92c737c501b32ba676a633de3b46d7306c1955b2
DanielPinnington <danielpinnington12@gmail.com>
2020-04-07 14:00:44 +0000

    Implemented draw function and testing 2D arrays for shape implementation
    Testing 2D arrays and draw function to represent the tetris shapes. Implemented an offset which will be used later for the positioning of the player shapes

--- Tetris.js
+++ Tetris.js
@@ -1,5 +1,34 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+context.scale(20, 20);
 context.fillStyle = '#000';
 context.fillRect(0,0,canvas.width, canvas.height);
+
+const matrix = [
+  [0,0,0], //0,0,0 here as it's easier to determine middle row.
+  [1,1,1], //This 2d array represents a tetris shape.
+  [0,1,0],
+];
+
+function draw(){
+  drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset){
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0){
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+const player = {
+  pos: {x: 5, y: 5},
+  matrix: matrix,
+}
+
+draw();
