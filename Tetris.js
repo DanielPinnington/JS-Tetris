f8cba6b8a1e06a8e4f1e804ef88ac52dc095b190
DanielPinnington <danielpinnington12@gmail.com>
2020-04-08 13:22:07 +0000

    Shapes dropping one a second
    Shapes fall down on the Y axis every second, main functionality of the game.

--- Tetris.js
+++ Tetris.js
@@ -26,14 +26,28 @@ function drawMatrix(matrix, offset){
   });
 }
 
-function update(){
+let lastTime = 0;
+let dropCounter = 0;
+let dropInterval = 1000;
+
+function update(time = 0){
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if(dropCounter > dropInterval){
+    player.pos.y++;
+    dropCounter = 0;
+  }
   draw();
   requestAnimationFrame(update);
 }
-
 const player = {
   pos: {x: 5, y: 5},
   matrix: matrix,
 }
 
+document.addEventListener('keydown', event =>{
+  console.log(event);
+});
 update();
